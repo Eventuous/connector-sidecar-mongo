807e4ff761024bbf3c3c89a4dbc91e53aa7c16a5
Alexey Zimarev <alex@zimarev.com>
2022-05-10 14:01:33 +0200

    Simplify

--- common.ts
+++ common.ts
@@ -1,11 +1,10 @@
 import * as grpc from "@grpc/grpc-js";
 import {Ignore, ProjectionRequest, ProjectionResponse, ProjectionServer, ProjectionService} from "./compiled/proto/project";
 import {Any} from "./compiled/google/protobuf/any";
-import {messageTypeRegistry} from "./compiled/typeRegistry";
-import {ValidResult} from "./mongoProjectors";
+import {messageTypeRegistry, UnknownMessage} from "./compiled/typeRegistry";
 
 export interface EventHandler<T> {
-    (event: T): Any;
+    (event: T): UnknownMessage;
 }
 
 interface EventHandlerMap<T> {
@@ -13,11 +12,11 @@ interface EventHandlerMap<T> {
     handler: EventHandler<T>;
 }
 
-export type AnyEventHandlerMap = EventHandlerMap<any>;
+export type AnyEventHandler = EventHandlerMap<any>;
 
-export type Projector = AnyEventHandlerMap[];
+export type Projector = AnyEventHandler[];
 
-export function WrapToAny(event: ValidResult): Any {
+export function WrapToAny(event: UnknownMessage): Any {
     return Any.fromPartial({
         typeUrl: `/${event.$type}`,
         value: messageTypeRegistry.get(event.$type)!.encode(event).finish()
@@ -44,7 +43,7 @@ const getProjectionServer = (projector: Projector): ProjectionServer => {
                         console.log(`(server) Invoking handler for event: ${ctx.eventType}`);
                         const result = ProjectionResponse.fromPartial({
                             eventId: ctx.eventId,
-                            operation: handler(ctx.eventPayload),
+                            operation: WrapToAny(handler(ctx.eventPayload)),
                             metadata: {}
                         });
                         console.log(`(server) Responding with: ${result.operation?.$type}`);
--- mongoProjectors.ts
+++ mongoProjectors.ts
@@ -1,23 +1,16 @@
-import {AnyEventHandlerMap, EventHandler, WrapToAny} from "./common";
-import {DeleteOne, Ignore, InsertOne, UpdateOne} from "./compiled/proto/project";
+import {AnyEventHandler, EventHandler} from "./common";
+import {DeleteOne, InsertOne, UpdateOne} from "./compiled/proto/project";
 
-export type ValidResult = Ignore | InsertOne | UpdateOne | DeleteOne;
-
-export function project<T>(eventType: string, handler: EventHandler<T>): AnyEventHandlerMap {
+function project<T>(eventType: string, handler: EventHandler<T>): AnyEventHandler {
     // @ts-ignore
     return {eventType, handler: x => handler(x as T)};
 }
 
-export const insertOne = <T>(eventType: string, handler: (event: T) => { document: object }): AnyEventHandlerMap => {
-    return project<T>(eventType, e => {
-        const result = InsertOne.fromPartial(handler(e));
-        return WrapToAny(result);
-    })
-};
+export const insertOne = <T>(eventType: string, handler: (event: T) => { document: object }) =>
+    project<T>(eventType, e => InsertOne.fromPartial(handler(e)));
 
-export const updateOne = <T>(eventType: string, handler: (event: T) => { filter: object, update: object }): AnyEventHandlerMap => {
-    return project<T>(eventType, e => {
-        const result = UpdateOne.fromPartial(handler(e));
-        return WrapToAny(result);
-    })
-};
+export const updateOne = <T>(eventType: string, handler: (event: T) => { filter: object, update: object }) =>
+    project<T>(eventType, e => UpdateOne.fromPartial(handler(e)));
+
+export const deleteOne = <T>(eventType: string, handler: (event: T) => { filter: object }) =>
+    project<T>(eventType, e => DeleteOne.fromPartial(handler(e)));
